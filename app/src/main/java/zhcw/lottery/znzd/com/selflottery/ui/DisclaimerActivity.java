f4d3b4532c766c05aaa4c1743b4b18c3bc8000f5
徐鹏展 <xupz@anmed.com.cn>
2020-10-09 16:43:47 +0800

    时间编码修改

--- app/src/main/java/zhcw/lottery/znzd/com/selflottery/ui/DisclaimerActivity.java
+++ app/src/main/java/zhcw/lottery/znzd/com/selflottery/ui/DisclaimerActivity.java
@@ -39,11 +39,10 @@ public class DisclaimerActivity extends BaseActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            //重写ToolBar返回按钮的行为，防止重新打开父Activity重走生命周期方法
-            case android.R.id.home:
-                finish();
-                return true;
+        //重写ToolBar返回按钮的行为，防止重新打开父Activity重走生命周期方法
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
